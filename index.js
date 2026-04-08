40c9ea67755fb39d461ebc66787bce5faadbc5ec
Sebastian Mejia <sebastianmejia@MacBook-Pro-de-Sebastian.local>
2026-04-08 13:19:35 -0500

    Parte 4: imagen publicada en Docker Hub

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const http = require('http');
+const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end('Hola desde bind mount! 🚀');
+  res.end("Hola desde mi imagen Docker 🚀");
 });
 
 server.listen(3000, () => {
-  console.log('Servidor en puerto 3000');
+  console.log("Servidor en puerto 3000");
 });
